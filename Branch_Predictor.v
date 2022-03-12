4b35c2cded78cae6f864678cc3aa07dc97b5c1ae
mehmetsakiraslan <87070594+mehmetsakiraslan@users.noreply.github.com>
2022-03-12 16:47:50 +0300

    Add files via upload

---
+++ Branch_Predictor.v
@@ -0,0 +1,90 @@
+`timescale 1ns / 1ps
+
+
+module Branch_Predictor(
+input i_reset,
+input i_saat,
+input [31:0] i_buyruk_sayaci,
+output o_buyruk_ongoru,
+
+input i_buyruk_atladi, // 1'b1 -> bir onceki dallanma atladi, 1'b0 -> bir onceki dallanma atlamadi. 
+output o_ongoru_yanlis
+    );
+    
+    /////////////////////////////////////////////////////////////
+    //  32 giriþli öngörü tablosu: 2'b00 -> G.T, 2'b11 -> G.A  //
+    /////////////////////////////////////////////////////////////                                                         
+    reg [1:0] gshare_table       [31:0];
+    reg [1:0] gshare_table_next  [31:0];
+    
+    reg [4:0] branch_history_table, branch_history_table_next; // Bir oncek, buyrugun aldigi degere gore guncellenmeli, 
+                                                               // Yani bir onceki cevrimdeki degerine de ihtiyac olabilir.
+                                                               // Yukaridaki gibi yapmak yerine bir onceki cevrimde erisilen tablo adresini tutuyorum.  
+    // Cikis registerlari degerleri.
+    reg r_buyruk_ongoru, r_buyruk_ongoru_next   ;
+    assign o_buyruk_ongoru = r_buyruk_ongoru    ; 
+    
+    reg r_ongoru_yanlis, r_ongoru_yanlis_next   ;
+    assign o_ongoru_yanlis = r_ongoru_yanlis    ;
+    
+    // Bir onceki buyruk sayacinin degeri tutulmali, 
+    // Veya onceki dallanma buyrugunun tabloda nereye eriþtigi  ve oncekinin dallanma buyrugu olup olmadigi tutulmali  
+    // farkli sekillerde yapilabilir. 
+    reg [4:0] onceki_buyruk_erisim, onceki_buyruk_erisim_next;
+    reg onceki_buyruk_dallanma, onceki_buyruk_dallanma_next;
+    
+    // Gelen buyrugun dallanma olup olmadigina karar veren wire. 
+    wire  yeni_dallanma_buyrugu; 
+    assign yeni_dallanma_buyrugu = i_buyruk_sayaci[1] && i_buyruk_sayaci[2]; // hangi buyruklarin gelebileceðine gore karar ver. 
+    
+    // Bir onceki buyrugun tabloda eristigi yeri tutan wire. 
+    wire yeni_ongoru_adresi; 
+    assign yeni_ongoru_adresi = branch_history_table[4:0] ^ buyruk_sayaci[6:2];
+    
+    reg [4:0] loop_counter; 
+    always@* begin
+        for(loop_counter=0 ; loop_counter < 32 ; loop_counter = loop_counter + 5'b1) begin  //
+            gshare_table_next[loop_counter] = gshare_table[loop_counter] ;                  //  Lacth olusturmamak icin bir if bloguna girilmedigi durumda tabloya eski degeri ataniyor
+        end                                                                                 //
+         
+        r_buyruk_ongoru_next = r_buyruk_ongoru  ;
+        r_ongoru_yanlis_next = r_ongoru_yanlis  ;      
+        branch_history_table_next = branch_history_table;
+        
+        if(onceki_buyruk_dallanma) begin                                                //
+            branch_history_table_next = {branch_history_table[4:0], i_buyruk_atladi};   // Genel geçmiþ tablosunun güncellenmesi 
+                                                                                        //
+            gshare_table_next[onceki_buyruk_erisim] = gshare_table_next[onceki_buyruk_erisim]  + 
+                                                                i_buyruk_atladi ? (1'b1) : (-1'b1); //Not: 2 bit counter saturated mi?                                                                           
+        end                                                                             
+        
+        if(yeni_dallanma_buyrugu) begin
+            r_buyruk_ongoru = gshare_table[yeni_ongoru_adresi][0];
+            
+            onceki_buyruk_erisim_next = yeni_ongoru_adresi; 
+            onceki_buyruk_dallanma_next = 1'b1; 
+        end
+    end
+    
+    always@(posedge i_saat) begin
+        if(i_reset) begin
+            for(loop_counter=0 ; loop_counter < 32 ; loop_counter = loop_counter + 5'b1) begin  //
+                gshare_table[loop_counter] = 2'b00 ;// Sayaclarin ilk tahmini ne?
+            end  
+            
+            r_buyruk_ongoru = 1'b0;
+            r_ongoru_yanlis = 1'b0;      
+            branch_history_table = 5'b0;
+        end
+        else begin
+             for(loop_counter=0 ; loop_counter < 32 ; loop_counter = loop_counter + 5'b1) begin  
+                gshare_table[loop_counter] = gshare_table_next[loop_counter] ; // Sayaclarin ilk tahmini ne?
+            end
+        
+            r_buyruk_ongoru = r_buyruk_ongoru_next;
+            r_ongoru_yanlis = r_ongoru_yanlis_next;      
+            branch_history_table = branch_history_table_next;
+        end
+    end 
+    
+endmodule
---
+++ Fetch_Decode.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.03.2022 14:27:22
+// Design Name: 
+// Module Name: Fetch_Decode
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Fetch_Decode(
+
+    );
+endmodule
