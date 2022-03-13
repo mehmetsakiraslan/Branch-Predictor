5bfc90e5e7ff4a294754d78bcda3931e35e4fa1b
mehmetsakiraslan <87070594+mehmetsakiraslan@users.noreply.github.com>
2022-03-13 17:42:53 +0300

    Update Branch_Predictor.v

--- Branch_Predictor.v
+++ Branch_Predictor.v
@@ -39,10 +39,10 @@ output o_ongoru_yanlis
     assign yeni_dallanma_buyrugu = i_buyruk[1] && i_buyruk[2]; // hangi buyruklarin gelebileceğine gore karar ver. 
     
     // Bir onceki buyrugun tabloda eristigi yeri tutan wire. 
-    wire yeni_ongoru_adresi; 
-    assign yeni_ongoru_adresi = branch_history_table[4:0] ^ buyruk_sayaci[6:2];
+    wire [4:0] yeni_ongoru_adresi; 
+    assign yeni_ongoru_adresi = branch_history_table_next[4:0] ^ i_buyruk_sayaci[6:2];
     
-    reg [4:0] loop_counter; 
+    reg [5:0] loop_counter; 
     always@* begin
         for(loop_counter=0 ; loop_counter < 32 ; loop_counter = loop_counter + 5'b1) begin  //
             gshare_table_next[loop_counter] = gshare_table[loop_counter] ;                  //  Lacth olusturmamak icin bir if bloguna girilmedigi durumda tabloya eski degeri ataniyor
@@ -52,15 +52,18 @@ output o_ongoru_yanlis
         r_ongoru_yanlis_next = r_ongoru_yanlis  ;      
         branch_history_table_next = branch_history_table;
         
+        onceki_buyruk_dallanma_next = onceki_buyruk_dallanma;
+        onceki_buyruk_erisim_next = onceki_buyruk_erisim;
+        
         if(onceki_buyruk_dallanma) begin                                                //
             branch_history_table_next = {branch_history_table[4:0], i_buyruk_atladi};   // Genel geçmiş tablosunun güncellenmesi 
                                                                                         //
             gshare_table_next[onceki_buyruk_erisim] = gshare_table_next[onceki_buyruk_erisim]  + 
-                                                                i_buyruk_atladi ? (1'b1) : (-1'b1); //Not: 2 bit counter saturated mi?                                                                           
+                                                                i_buyruk_atladi ? (2'b1) : (- 2'b1); //Not: 2 bit counter saturated mi?                                                                           
         end                                                                             
         
         if(yeni_dallanma_buyrugu) begin
-            r_buyruk_ongoru = gshare_table[yeni_ongoru_adresi][0];
+            r_buyruk_ongoru_next = gshare_table[yeni_ongoru_adresi][0];
             
             onceki_buyruk_erisim_next = yeni_ongoru_adresi; 
             onceki_buyruk_dallanma_next = 1'b1; 
@@ -70,21 +73,27 @@ output o_ongoru_yanlis
     always@(posedge i_saat) begin
         if(i_reset) begin
             for(loop_counter=0 ; loop_counter < 32 ; loop_counter = loop_counter + 5'b1) begin  //
-                gshare_table[loop_counter] = 2'b00 ;
+                gshare_table[loop_counter] <= 2'b00 ;
             end  
             
-            r_buyruk_ongoru = 1'b0;
-            r_ongoru_yanlis = 1'b0;      
-            branch_history_table = 5'b0;
+            r_buyruk_ongoru <= 1'b0;
+            r_ongoru_yanlis <= 1'b0;      
+            branch_history_table <= 5'b0;
+            
+            onceki_buyruk_dallanma <= 1'b0;
+            onceki_buyruk_erisim <= 5'b0;
         end
         else begin
              for(loop_counter=0 ; loop_counter < 32 ; loop_counter = loop_counter + 5'b1) begin  
-                gshare_table[loop_counter] = gshare_table_next[loop_counter] ; // Sayaclarin ilk tahmini ne?
+                gshare_table[loop_counter] <= gshare_table_next[loop_counter] ; // Sayaclarin ilk tahmini ne?
             end
         
-            r_buyruk_ongoru = r_buyruk_ongoru_next;
-            r_ongoru_yanlis = r_ongoru_yanlis_next;      
-            branch_history_table = branch_history_table_next;
+            r_buyruk_ongoru <= r_buyruk_ongoru_next;
+            r_ongoru_yanlis <= r_ongoru_yanlis_next;      
+            branch_history_table <= branch_history_table_next;
+            
+            onceki_buyruk_dallanma <= onceki_buyruk_dallanma_next;
+            onceki_buyruk_erisim <= onceki_buyruk_erisim_next;
         end
     end 
     
